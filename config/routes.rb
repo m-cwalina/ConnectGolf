41c02d336766fba9e352b3547f7ef75ed923832c
Matthew Cwalina <matthewcwalina@gmail.com>
2022-08-02 17:23:35 +0200

    Created a friends controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
   devise_for :users
   devise_scope :user do
     get '/user' => 'dashboards#user_dashboard', :as => :user_dashboard
   end
   root to: 'pages#home', as: :home
   resources :users, only: [:index]
+  resources :friends, only: [:create]
 end
