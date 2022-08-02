3414dd6203cef990f5240b5d3796613be100dec0
Matthew Cwalina <matthewcwalina@gmail.com>
2022-08-02 14:20:07 +0200

    Created seeds for club and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying all Clubs and Users'
+User.destroy_all
+Club.destroy_all
+
 puts 'Creating clubs'
-Club.create!()
+Club.create!(name: 'Santa Ana Country Club', address: '20382 Newport Blvd, Santa Ana, CA 92707, United States')
 puts 'Creating All Users'
 50.times {User.create!(email: "#{Faker::Vehicle.manufacture}@gmail.com", name: Faker::Name.name, password: 'password', age: rand(15..100), handicap: rand(0..35), club:)}
