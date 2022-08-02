12a5c85d807742769c43d5a1a0ac2ce0f0153daf
Matthew Cwalina <matthewcwalina@gmail.com>
2022-08-02 12:56:35 +0200

    Added all dependencies in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :friends, through: :map_user_to_friends
+  has_many :groups, through: :tee_times
+  belongs_to :club
 end
