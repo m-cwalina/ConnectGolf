29dfe1ea6f5276954019a4ed8eec0bb9ac1ee863
Matthew Cwalina <matthewcwalina@gmail.com>
2022-08-03 14:21:56 +0200

    Dropped the table friends

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_03_121049) do
+ActiveRecord::Schema.define(version: 2022_08_03_121828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,11 +20,6 @@ ActiveRecord::Schema.define(version: 2022_08_03_121049) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "address"
-  end
-
-  create_table "friends", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "friendships", force: :cascade do |t|
