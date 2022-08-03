63302b461506fa5785ac4cf94cdcdec9a2257302
Matthew Cwalina <matthewcwalina@gmail.com>
2022-08-03 14:44:16 +0200

    changed false value away from symbol

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_03_123559) do
+ActiveRecord::Schema.define(version: 2022_08_03_123957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_08_03_123559) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "friend_id"
+    t.boolean "confirmed", default: false
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
