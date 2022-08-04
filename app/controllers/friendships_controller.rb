b394022896e54df62234a7ac796405f7f8285c68
Matthew Cwalina <matthewcwalina@gmail.com>
2022-08-04 21:26:51 +0200

    Friendship create method working

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,8 +1,22 @@
 class FriendshipsController < ApplicationController
   def create
-    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+    @user = current_user
+    @friendship = @user.friendships.build(friend_id: params[:friend_id])
     if @friendship.save
       redirect_to user_dashboard_path
+    else
+      redirect_to home_path
     end
+  end
+
+  def index
+    @friendships = Friendship.all
+  end
+
+  def update
+    @friendship = Friendship.find(params[:id])
+    @friendship.status = "accepted"
+    @friendship.save
+    redirect_to user_dashboard_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
-    get '/user' => 'dashboards#user_dashboard', :as => :user_dashboard
+    get '/user' => 'dashboards#user_dashboard', as: :user_dashboard
   end
   root to: 'pages#home', as: :home
   resources :users, only: [:index]
-  resources :friendships, only: [:create]
+  resources :friendships, only: %i[create index update]
 end
