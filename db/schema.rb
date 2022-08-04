3da49a849c2fdb5c6c24a0869fa3a485533e8c5e
Matthew Cwalina <matthewcwalina@gmail.com>
2022-08-04 16:40:20 +0200

    Added status to friendship table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_143613) do
+ActiveRecord::Schema.define(version: 2022_08_04_143943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_08_04_143613) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "friend_id"
+    t.string "status"
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
