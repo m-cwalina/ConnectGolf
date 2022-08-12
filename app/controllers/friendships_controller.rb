028413c12ceb135b45e6991791c4b51b656e4e04
Matthew Cwalina <matthewcwalina@gmail.com>
2022-08-12 14:47:28 +0200

    Changed friendship index

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -7,7 +7,7 @@ class FriendshipsController < ApplicationController
   end
 
   def index
-    @friendships = Friendship.where(user_id: current_user.id)
+    @friendships = current_user.friendships.all
   end
 
   def update
@@ -20,12 +20,3 @@ class FriendshipsController < ApplicationController
     end
   end
 end
-
-  # rounded = (2.3.to_f/0.5).round * 0.5
-  # fulls = rounded.to_i
-  # full_star = fulls.times { puts 'full'}
-  # halfs = (rounded - rounded.to_i)\0.5
-  # half_star = halfs.to_i.times { puts 'half'}
-  # emptys = 5 - fulls - halfs
-  # empty_star = emptys.to_i.times { puts 'empty'}
-  # string = `#{full_star} + #{half_star} + #{empty_star}`
