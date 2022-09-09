86cba00c4e3bf4627a065a515aa6eb226f9d98af
Matthew Cwalina <matthewcwalina@gmail.com>
2022-09-09 11:46:21 +0200

    Added status integer to booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_07_154642) do
+ActiveRecord::Schema.define(version: 2022_09_09_094515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_09_07_154642) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "size"
+    t.integer "status", default: 0
     t.index ["tee_time_id"], name: "index_bookings_on_tee_time_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
