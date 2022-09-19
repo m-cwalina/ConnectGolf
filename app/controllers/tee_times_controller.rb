f3b131585ff0c5cd6da769d75179350f5c49246f
Matthew Cwalina <matthewcwalina@gmail.com>
2022-09-19 16:19:52 +0200

    Added an API for Tee Times

--- app/controllers/tee_times_controller.rb
+++ app/controllers/tee_times_controller.rb
@@ -1,11 +1,4 @@
 class TeeTimesController < ApplicationController
-
   def index
-    start_date = params.fetch(:start_date, Date.today).to_date
-    @tee_times = TeeTime.available_teetime.where(start_time: start_date..Date.today + 14.days).order(:created_at)
-  end
-
-  def show
-    @tee_time = TeeTime.find(params[:id])
   end
 end
