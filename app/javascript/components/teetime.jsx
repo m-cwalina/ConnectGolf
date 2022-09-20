c37682c37d6c4a33e4618767b72b00820baece4b
Matthew Cwalina <matthewcwalina@gmail.com>
2022-09-20 16:29:33 +0200

    Updated tiles for tee times

--- app/javascript/components/teetime.jsx
+++ app/javascript/components/teetime.jsx
@@ -4,8 +4,8 @@ export default function TeeTime(props) {
   return (
     <div>
       <div className="tile">
-        <p className="teetime-name">{props.teetime.time}</p>
-        <p className="players">{props.teetime.players}</p>
+        <p className="tile-info">{props.teetime.time}</p>
+        <p className="tile-info">{props.teetime.players}</p>
       </div>
     </div>
   )
