8c09c65ea9aa96af92e1d2b8ad98aaea25c78943
Matthew Cwalina <matthewcwalina@gmail.com>
2022-09-23 16:01:41 +0200

    Added fontawesome to react pages

--- app/javascript/components/teetime.jsx
+++ app/javascript/components/teetime.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { format, parseISO } from 'date-fns';
-import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export default function TeeTime(props) {
   return (
     <div>
       <div className="tile">
-        <p className="tile-info">{format(parseISO(props.teetime.time), 'h:mm a')}</p>
+
+
+
+        <FontAwesomeIcon icon="fa-solid fa-timer"/><p className="tile-info">{format(parseISO(props.teetime.time), 'h:mm a')}</p>
         <p className="tile-info">{format(parseISO(props.teetime.start_time), 'PP')}</p>
         <p className="tile-info">{props.teetime.players} players</p>
         <a className="tile-info" href={`/tee_times/${props.teetime.id}/bookings/new`}>
-          <button className="btn btn-primary btn-lg">Book</button>
+          <button className="btn btn-outline-success btn-lg">Book</button>
         </a>
       </div>
     </div>
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -2,24 +2,12 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from './app';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
 import './react.scss';
-
-const router = createBrowserRouter([
-  {
-    path: "/tee_times",
-  },
-]);
 
 // render an instance of the component in the DOM
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 );
