1c0f8487fca78796033700226ec8ab7fd9218e92
Matthew Cwalina <matthewcwalina@gmail.com>
2022-09-23 17:44:37 +0200

    Updated seed file

--- app/javascript/components/teetime.jsx
+++ app/javascript/components/teetime.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { format, parseISO } from 'date-fns';
-
+import { IoAlarm, IoFlagOutline, IoGolf } from "react-icons/io5";
 export default function TeeTime(props) {
   return (
     <div>
       <div className="tile">
-        <p className="tile-info">{format(parseISO(props.teetime.time), 'h:mm a')}</p>
-        <p className="tile-info">{format(parseISO(props.teetime.start_time), 'PP')}</p>
-        <p className="tile-info">{props.teetime.players} players</p>
+        <p className="tile-info"><IoAlarm /> {format(parseISO(props.teetime.time), 'h:mm a')}</p>
+        <p className="tile-info"><IoFlagOutline/> {format(parseISO(props.teetime.start_time), 'PP')}</p>
+        <p className="tile-info"><IoGolf/> {props.teetime.players} players</p>
         <a className="tile-info" href={`/tee_times/${props.teetime.id}/bookings/new`}>
           <button className="btn btn-outline-success btn-lg">Book</button>
         </a>
