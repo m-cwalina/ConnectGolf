36b24b230fa64b0502a52c7746e2f73f4168bd81
Matthew Cwalina <matthewcwalina@gmail.com>
2022-09-23 12:42:26 +0200

    Tee time booking system working

--- app/javascript/components/teetime.jsx
+++ app/javascript/components/teetime.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { format, parseISO } from 'date-fns';
+import { Link } from "react-router-dom";
 
 export default function TeeTime(props) {
   return (
     <div>
       <div className="tile">
         <p className="tile-info">{format(parseISO(props.teetime.time), 'h:mm a')}</p>
+        <p className="tile-info">{format(parseISO(props.teetime.start_time), 'PP')}</p>
         <p className="tile-info">{props.teetime.players} players</p>
+        <a className="tile-info" href={`/tee_times/${props.teetime.id}/bookings/new`}>
+          <button className="btn btn-primary btn-lg">Book</button>
+        </a>
       </div>
     </div>
   )
