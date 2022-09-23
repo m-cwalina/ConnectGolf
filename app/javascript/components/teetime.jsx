0925fc26fcec43e1273d4933da424ad519b140bf
Matthew Cwalina <matthewcwalina@gmail.com>
2022-09-23 16:39:37 +0200

    Added react icons to project

--- app/javascript/components/teetime.jsx
+++ app/javascript/components/teetime.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import { format, parseISO } from 'date-fns';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 
 export default function TeeTime(props) {
   return (
     <div>
       <div className="tile">
-
-
-
-        <FontAwesomeIcon icon="fa-solid fa-timer"/><p className="tile-info">{format(parseISO(props.teetime.time), 'h:mm a')}</p>
+        <p className="tile-info">{format(parseISO(props.teetime.time), 'h:mm a')}</p>
         <p className="tile-info">{format(parseISO(props.teetime.start_time), 'PP')}</p>
         <p className="tile-info">{props.teetime.players} players</p>
         <a className="tile-info" href={`/tee_times/${props.teetime.id}/bookings/new`}>
