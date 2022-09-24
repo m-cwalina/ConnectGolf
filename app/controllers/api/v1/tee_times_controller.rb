573d9e67e6812dd830af43afabc2f677b6214578
Matthew Cwalina <matthewcwalina@gmail.com>
2022-09-24 20:39:16 +0200

    Changed range of dates for tee times

--- app/controllers/api/v1/tee_times_controller.rb
+++ app/controllers/api/v1/tee_times_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TeeTimesController < ApplicationController
   def index
     start_date = params.fetch(:start_date, Date.today).to_date
-    @tee_times = TeeTime.available_teetime.where(start_time: start_date..Date.today + 14.days).order(:created_at)
+    @tee_times = TeeTime.available_teetime.where(start_time: start_date..Date.today + 13.days).order(:created_at)
   end
 end
