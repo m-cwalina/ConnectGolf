2e682458d747299a2ac2799ef3ab09e09c64e287
Matthew Cwalina <matthewcwalina@gmail.com>
2022-11-02 13:16:33 +0100

    Changed the sql query for tee times

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def user_dashboard
     @user = current_user
-    @tee_times = @user.tee_times
+    @tee_times = @user.tee_times.order(time: :asc).where('time > ?', DateTime.now )
   end
 end
