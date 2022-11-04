8f485ba808ae5c8c6988e9065ca5955c891eb1c1
Matthew Cwalina <matthewcwalina@gmail.com>
2022-11-04 15:33:06 +0100

    Added a no tee time component to render when teetimes.length is null

--- app/javascript/components/no_teetime.jsx
+++ app/javascript/components/no_teetime.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import BlankImage from 'images/blankpage-logo.png'
 
 export default function NoTeeTime(props) {
   return (
-    <div>
-      <h1>There are no more tee times today.</h1>
+    <div className='no-teetime-container'>
+      <div><img className="blankpage-logo" src={BlankImage}></img></div>
+      <div><h3 style={{color: '#818181'}} >There are no more tee times today.</h3></div>
     </div>
   )
 }
--- app/javascript/components/teetimes.jsx
+++ app/javascript/components/teetimes.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import TeeTime from './teetime';
+import NoTeeTime from '../components/no_teetime'
 
 export default function TeeTimes(props) {
 
   const renderList = () => {
-    return props.teetimes.map((teetime) => {
-      return (
-        <div key={teetime.id} className="tiles">
-          <div><TeeTime teetime={teetime}/></div>
-        </div>
-      );
-    });
+    if (props.teetimes.length >= 1) {
+      return props.teetimes.map((teetime) => {
+        return (
+          <div key={teetime.id} className="tiles">
+            <div><TeeTime teetime={teetime}/></div>
+          </div>
+        );
+      });
+    } else {
+      return <NoTeeTime />
+    }
   };
 
   return (
