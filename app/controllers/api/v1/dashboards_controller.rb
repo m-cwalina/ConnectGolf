3931333b61ba79d46f18cf9fac4f19d251399b94
Matthew Cwalina <matthewcwalina@gmail.com>
2022-11-14 17:08:22 +0100

    Updated sql queries to sort by dates

--- app/controllers/api/v1/dashboards_controller.rb
+++ app/controllers/api/v1/dashboards_controller.rb
@@ -1,6 +1,4 @@
 class Api::V1::DashboardsController < ApplicationController
-
-
   def hourly
     # This provides the amount of bookings per hour
     @teetimes = TeeTime.between(Date.today, Date.tomorrow).joins(:bookings).group("date_trunc('hour', time)").count
@@ -8,22 +6,21 @@ class Api::V1::DashboardsController < ApplicationController
 
   def daily
     # This provides the amount of bookings per day
-    @teetimes = TeeTime.between(Date.today.beginning_of_month, Date.today.end_of_month).joins(:bookings).group('time::date').count
+    @teetimes = TeeTime.between(Date.today, Date.today + 30.days).joins(:bookings).group("date_trunc('day', time)").count.sort_by { |k, _| k }.to_h
   end
 
   def weekly
     # Provides the amount of bookings per week
-    @teetimes = TeeTime.joins(:bookings).group("date_trunc('week', time)").count
+    @teetimes = TeeTime.joins(:bookings).group("date_trunc('week', time)").count.sort_by { |k, _| k }.to_h
   end
 
   def monthly
-    #This provides the amount of bookings per month
-    @teetimes = TeeTime.joins(:bookings).group("date_trunc('month', time)").count
+    # This provides the amount of bookings per month
+    @teetimes = TeeTime.joins(:bookings).group("date_trunc('month', time)").count.sort_by { |k, _| k }.to_h
   end
 
   def yearly
-    #This provides the amount of bookings per month
+    # This provides the amount of bookings per month
     @teetimes = TeeTime.joins(:bookings).group("date_trunc('year', time)").count
   end
-
 end
