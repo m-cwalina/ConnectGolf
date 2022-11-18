b642db4f9405aa9de54638bb02d403f6e2eca6f2
Matthew Cwalina <matthewcwalina@gmail.com>
2022-11-18 15:20:22 +0100

    Added scss to teesheet page

--- app/javascript/components/teesheet/teesheet.jsx
+++ app/javascript/components/teesheet/teesheet.jsx
@@ -40,12 +40,18 @@ export default function TeeSheet() {
     return searchTeeTimes.map((teetime) => {
       return (
         <ul className='teesheet' key={teetime.id}>
-          <div className='teesheet-icon'><IoTimerOutline /></div>
-          <div className='teesheet-info'> {format(parseISO(teetime.time), 'h:mm a')}</div>
-          <div className='teesheet-icon'><IoPeopleOutline /></div>
-          <div className='teesheet-info'> {(teetime.players.length > 0) ? (teetime.players.map(player => player.size)) : ("0")} players</div>
-          <div className='teesheet-icon'><IoPersonOutline /></div>
-          <div className='teesheet-info'> {(teetime.users.length > 0) ? (teetime.users.map(user => user.name)) : ("No Players")}</div>
+          <div className='teesheet-total'>
+            <div className='teesheet-icon'><IoTimerOutline /></div>
+            <div className='teesheet-info'> {format(parseISO(teetime.time), 'h:mm a')}</div>
+          </div>
+          <div className='teesheet-total'>
+            <div className='teesheet-icon'><IoPeopleOutline /></div>
+            <div className='teesheet-info'> {(teetime.players.length > 0) ? (teetime.players.map(player => player.size)) : ("0")} players</div>
+          </div>
+          <div className='teesheet-total'>
+            <div className='teesheet-icon'><IoPersonOutline /></div>
+            <div className='teesheet-info'> {(teetime.users.length > 0) ? (teetime.users.map(user => user.name)) : ("No Players")}</div>
+          </div>
           <div className='teesheet-info'>
             <Form method='post'>
               <input type="hidden" name='friend_id' value={teetime.id} />
