284706484cd645074589d3f3ff2e35090d1ac39e
Matthew Cwalina <matthewcwalina@gmail.com>
2022-11-28 16:26:42 +0100

    Added group join table

--- db/migrate/20220907112421_drop_groups.rb
+++
@@ -1,7 +0,0 @@
-class DropGroups < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :groups do |t|
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20221128152603_create_groups.rb
+++ db/migrate/20221128152603_create_groups.rb
@@ -1,6 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[6.1]
   def change
     create_table :groups do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :booking, null: false, foreign_key: true
 
       t.timestamps
     end
