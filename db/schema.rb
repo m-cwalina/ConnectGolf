898039b2be35c9e611db5c34598a486970b9275d
Matthew Cwalina <matthewcwalina@gmail.com>
2022-11-28 16:27:17 +0100

    Migrated group table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_17_171106) do
+ActiveRecord::Schema.define(version: 2022_11_28_152603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2022_11_17_171106) do
     t.integer "friend_id"
     t.string "status"
     t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_groups_on_booking_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -80,6 +89,8 @@ ActiveRecord::Schema.define(version: 2022_11_17_171106) do
   add_foreign_key "bookings", "tee_times"
   add_foreign_key "bookings", "users"
   add_foreign_key "friendships", "users"
+  add_foreign_key "groups", "bookings"
+  add_foreign_key "groups", "users"
   add_foreign_key "posts", "clubs"
   add_foreign_key "users", "clubs"
 end
