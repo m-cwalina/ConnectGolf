aea5b98d2b051341517d0f4aa755266722701820
Matthew Cwalina <matthewcwalina@gmail.com>
2023-01-18 18:15:57 -0800

    Updated group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
+  belongs_to :player2, class_name: 'User'
+  belongs_to :player3, class_name: 'User'
+  belongs_to :player4, class_name: 'User'
   belongs_to :booking
 end
