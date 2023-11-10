3005082ff09652724f84887c12d5cd154505648c
Matthew Cwalina <matthewcwalina@gmail.com>
2023-11-10 16:23:05 +0100

    Moved tee_time function into the model

--- app/models/tee_time.rb
+++ app/models/tee_time.rb
@@ -3,4 +3,16 @@ class TeeTime < ApplicationRecord
   has_many :users, through: :bookings
   scope :between, lambda { |start_date, end_date| where('time >= ? AND time <= ?', start_date, end_date) }
   scope :available_teetime, -> { where('players >=1') }
+
+  def self.teetimes_for(period)
+    scope = case period
+            when 'hour'
+              self.between(Date.today, Date.tomorrow)
+            when 'day'
+              self.between(Date.today, Date.today + 30.days)
+            else
+              self.all
+            end
+    scope.joins(:bookings).group("date_trunc('#{period}', time)").count.sort_by { |k, _| k }.to_h
+  end
 end
